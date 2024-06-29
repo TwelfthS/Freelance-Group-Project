39f80afa93f077028ecd1e22122f876ce115bb08
twelfths <titov.sergey-2015@yandex.ru>
2024-06-29 17:53:46 +0300

    upd scene

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,24 +4,85 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public EnemyBattle enemyBattle;
+    // public EnemyBattle enemyBattle;
     public HPHandler hp;
+    public DetectionZone detectionZone;
+    public GameObject player;
+    public bool seesPlayer = false;
+    public Rigidbody2D rb;
+    public float moveSpeed = 5;
+    public int damage = 4;
+    public float attackCooldown = 0;
+    private float maxCooldown = 1;
     void Start()
     {
-        enemyBattle = GetComponent<EnemyBattle>();
+        // enemyBattle = GetComponent<EnemyBattle>();
         hp = GetComponent<HPHandler>();
+        detectionZone = GetComponent<DetectionZone>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (hp.isAlive) {
-            enemyBattle.Action();
+            Action();
         }
     }
 
     public void Hit(int damage) {
         Debug.Log("Hit?");
         hp.Hit(damage);
+    }
+
+    // public void StartCombat() {
+    //     inCombat = true;
+    // }
+
+    // public void StopCombat() {
+    //     inCombat = false;
+    // }
+
+    public void Action() {
+        if (seesPlayer) {
+            Cooldown();
+            float distance = Vector2.Distance(player.transform.position, transform.position);
+            if (distance < 3 && attackCooldown <= 0) {
+                AttackPlayer(damage);
+                attackCooldown = maxCooldown;
+            }
+            moveToPlayer(distance);
+        } else {
+            checkForPlayer();
+        }
+    }
+
+    private void Cooldown() {
+        if (attackCooldown > 0) {
+            attackCooldown -= Time.deltaTime;
+        }
+    }
+
+    private void moveToPlayer(float currentDistance) {
+        if (currentDistance > 3) {
+            Vector2 dir = player.transform.position - transform.position;
+            dir.Normalize();
+            rb.velocity = new Vector2(dir.x * moveSpeed, dir.y * moveSpeed);            
+        } else {
+            rb.velocity = Vector2.zero;
+        }
+
+    }
+
+    private void checkForPlayer() {
+        if (detectionZone.seesPlayer) {
+            seesPlayer = true;
+            player = detectionZone.player;
+            player.GetComponent<PlayerBattle>().SetEnemy(this.gameObject);
+        }
+    }
+
+    private void AttackPlayer(int damage) {
+        player.GetComponent<HPHandler>().Hit(damage);
+        Debug.Log("You got hit!");
     }
 }
